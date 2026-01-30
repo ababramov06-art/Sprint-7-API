60e7afd4d8a0a76a22cf1809b997ee5266ac5912
Александр Абрамов <aabramov06@yandex.ru>
2026-01-30 19:11:39 +0500

    Удаление курьера после теста.

--- urls.py
+++ urls.py
@@ -2,5 +2,7 @@ class Urls:
     URL_basic = 'https://qa-scooter.praktikum-services.ru/'
     URL_courier_create = f'{URL_basic}api/v1/courier/'
     URL_courier_login = f'{URL_basic}api/v1/courier/login'
+    URL_courier_delete = f'{URL_basic}/api/v1/courier/:'
     URL_orders_create = f'{URL_basic}api/v1/orders'
     URL_orders_cancel = f'{URL_basic}api/v1/orders/cancel'
+    
